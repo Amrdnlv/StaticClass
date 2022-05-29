15dd7b55c231bcca9ac40b02c4d99fbb9afb6e8c
Amrdnlv <onne.onne@mail.ru>
2022-05-29 14:21:38 +0300

    Добавьте файлы проекта.

---
+++ StaticClass/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Задание
+            Используя Visual Studio, создайте проект по шаблону Console Application.
+            Требуется:
+            Создать статический класс Calculator, с методами для выполнения основных арифметических
+            операций.
+            Написать программу, которая выводит на экран основные арифметические операции.*/
+            Console.WriteLine("Calculator");
+            Console.WriteLine(StaticClass.StaticCalculation.Umn(10, 5));
+            Console.WriteLine(StaticClass.StaticCalculation.Del(10, 5));
+            Console.WriteLine(StaticClass.StaticCalculation.Sum(10, 5));
+            Console.WriteLine(StaticClass.StaticCalculation.Minus(10, 5));
+        }
+    }
+}
---
+++ StaticClass/StaticCalculation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClass
+{
+     static class StaticCalculation
+    {
+        static public double Sum(double a, double b)
+        {
+            return a + b;
+        }
+        static public double Minus(double a, double b)
+        {
+            return a - b;
+        }
+        static public double Umn(double a, double b)
+        {
+            return a * b;
+        }
+        static public double Del(double a, double b)
+        {
+            if (b == 0) return 0;
+            return a / b;
+        }
+
+
+    }
+}
